83d654869a408e4582ed89342d24f96ea26a35d7
Kim <wukimberly1@gmail.com>
2024-06-08 08:12:28 -0400

    remove products data

--- Allergens/Sample.js
+++ Allergens/Sample.js
@@ -3,11 +3,11 @@ document.getElementById("products-link").addEventListener("click", function(even
     fetchProductsData();
 });
 
-const productsData = [
-    { id: 1, name: "Product 1", price: 10.99, description: "Description of Product 1" },
-    { id: 2, name: "Product 2", price: 15.99, description: "Description of Product 2" },
-    { id: 3, name: "Product 3", price: 20.99, description: "Description of Product 3" }
-];
+// const productsData = [
+//     { id: 1, name: "Product 1", price: 10.99, description: "Description of Product 1" },
+//     { id: 2, name: "Product 2", price: 15.99, description: "Description of Product 2" },
+//     { id: 3, name: "Product 3", price: 20.99, description: "Description of Product 3" }
+// ];
 
 
 function fetchProductsData() {
