739748a18c073a9ff43b63ac69ee232820e9f5dd
Kim <wukimberly1@gmail.com>
2024-06-14 18:57:53 -0400

    set up

--- #/script.js
+++ #/script.js
@@ -3,49 +3,59 @@ let currentState = "Standby";
 let Ans = 0;
 
 function onInput(event) {
-    let keyValue = event.target.textContent;
-    switch (keyValue) {
-        case "ON":
-            currentState = "Standby";
-            input.value = "";
-            break;
-        case "ANS":
-            onNumberPress(Ans);
-            break;
-        case "AC":
-            currentState = "Standby";
-            input.value = "";
-            Ans = 0;
-            break;
-        case "DEL":
-            input.value = input.value.slice(0, -1);
-            break;
-        default:
-            if (currentState === "Standby") {
-                input.value = keyValue;
-                currentState = "Input";
-            } else {
-                input.value += keyValue;
-            }
-    }
+  let keyValue = event.target.textContent;
+  switch (keyValue) {
+    case "ON":
+      currentState = "Standby";
+      input.value = "";
+      break;
+    case "ANS":
+      onNumberPress(Ans);
+      break;
+    case "AC":
+      currentState = "Standby";
+      input.value = "";
+      Ans = 0;
+      break;
+    case "DEL":
+      input.value = input.value.slice(0, -1);
+      break;
+    default:
+      if (currentState === "Standby") {
+        input.value = keyValue;
+        currentState = "Input";
+      } else {
+        input.value += keyValue;
+      }
+  }
 }
 
-function calculateResult() {
-    if (currentState === "Error" || currentState === "Standby") return;
-    let result = eval(input.value);
-    if (isNaN(result)) {
-        input.value = "Error";
-        currentState = "Error";
-    } else {
-        input.value = result;
-        Ans = result;
-        currentState = "Standby";
-    }
+function onNumberPress(number) {
+  if (currentState === "Standby") {
+    input.value = number;
+    currentState = "Input";
+  } else {
+    input.value += number;
+  }
 }
 
-document.addEventListener("keydown", onInput);
-let buttons = document.querySelectorAll("button");
-buttons.forEach(function (button) {
+function onOperatorPress(operator) {
+  if (currentState === "Input") {
+    input.value += operator;
+    currentState = "Operator";
+  }
+}
+
+function onEqualsPress() {
+  let result = eval(input.value);
+  input.value = result;
+  Ans = result;
+  currentState = "Standby";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.querySelectorAll("button");
+  buttons.forEach(function (button) {
     button.addEventListener("click", onInput);
+  });
 });
-document.getElementById("equals").addEventListener("click", calculateResult);
