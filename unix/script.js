dd5228bfd9abe6e0df580be562176a00c18f73fc
Kim <wukimberly1@gmail.com>
2024-06-15 13:43:46 -0400

    up

--- unix/script.js
+++ unix/script.js
@@ -1,1 +1,13 @@
-console.log("unxp page script loaded!");
+function generateUnixTime() {
+    const year = document.getElementById("year").value;
+    const month = document.getElementById("month").value;
+    const day = document.getElementById("day").value;
+    const hour = document.getElementById("hour").value;
+    const minute = document.getElementById("minute").value;
+    const second = document.getElementById("second").value;
+
+    const date = new Date(Date.UTC(year, month, day, hour, minute, second));
+    const unixTime = Math.floor(date.getTime() / 1000);
+
+    document.getElementById("unix-time").innerHTML = `Unix Time: ${unixTime}`;
+  }
